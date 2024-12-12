7db520a5ab1cd9d44e032464c219cdd02ff43060
socialwing02 <socialwing02@gmail.com>
2024-12-12 12:58:01 +0530

    about

--- src/pages/Screen/About.tsx
+++ src/pages/Screen/About.tsx
@@ -1,3 +1,39 @@
-export default function About() {
-  return <div>About</div>;
+import React from "react";
+import image from "../../assets/images/eng.png";
+
+function About() {
+  return (
+    // <div className="lg:items-center self-center justify-center ">
+      <div className="container px-4 py-4 min-h-screen mx-auto border-2 gap-12 flex flex-col sm:flex-row justify-center items-center ">
+        <div>
+          <img className="h-auto w-100 rounded-lg lg:w-[500px]	" src={image} />
+        </div>
+        <div className="text-center py-9 lg:w-[600px]">
+          <h1 className="text-xl ">I am a student</h1>
+          <h6 className="text-4xl py-2">I Can Learn English </h6>
+          <p className="m-2">
+            About us. Established in 1947, our language school has continuously
+            enhanced its methodology and efficiency, and has become a renowned
+            partner for those who want to improve their English language skills
+            in a fast and reliable way.
+          </p>
+
+          <p className="text-lx m-2">
+            About us. Established in 1947, our language school has continuously
+            enhanced  improve their English language skills
+            in a fast and reliable way.
+          </p>
+
+          <p className="m-2">
+            About us. Established in 1947, our language school has continuously
+            enhanced its methodology and efficiency, and has become a renowned
+            partner for those who want to improve their English language skills
+            in a fast and reliable way.
+          </p>
+          
+        </div>
+      </div>
+    // </div>
+  );
 }
+export default About;
